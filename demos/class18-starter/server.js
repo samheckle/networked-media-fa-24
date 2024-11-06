d04a8be9956c1e2896c3638e4f0f8cccd31246f7
samheckle <samheckle@yahoo.com>
2024-11-05 20:36:58 -0500

    added finished class 18 demo

--- demos/class18-starter/server.js
+++ demos/class18-starter/server.js
@@ -9,6 +9,7 @@
 const express = require("express"); // imports express
 const multer = require("multer");   // imports multer -- handles file upload
 const bodyParser = require("body-parser");  // imports body parser -- allows us to have a body in server request
+const nedb = require("@seald-io/nedb")
 
 // translates bits and bytes (literal memory and data) to something readable by the server
 const urlEncodedParser = bodyParser.urlencoded({ extended: true }); 
@@ -23,6 +24,11 @@ const upload = multer({
   dest: "public/uploads",
 });
 
+let database = new nedb({
+  filename: "database.txt",
+  autoload: true
+})
+
 // what do each of these statements do?
 app.use(express.static("public"));  // set the default folder for any static files such as assets, css, html
 app.use(urlEncodedParser);        // middleware to make sure the bits and bytes can be understood by the app
@@ -35,9 +41,36 @@ app.get("/", (request, response) => {
 
   // what steps do we need in order to use a template ejs file?
   // 
-  response.render('index.ejs', {})
+
+  let query = {}
+  let sortQuery = {
+    timestamp: -1 // sort in reverse chronological order
+  }
+
+  database.find(query).sort(sortQuery).exec( (err, retreivedData)=>{
+    response.render('index.ejs', {posts: retreivedData})
+  })
+
+  // response.render('index.ejs', {})
   // make sure to comment out the res.send() code above
 });
+
+app.post('/upload', upload.single('theimage'), (req, res)=>{
+  let currDate = new Date()
+  let data = {
+    text: req.body.text,
+    date: currDate.toLocaleString(),
+    timestamp: currDate.getTime()
+  }
+  if(req.file){
+    data.image = "/uploads/" + req.file.filename
+  }
+
+  database.insert(data, (err, newData)=>{
+    console.log(newData)
+    res.redirect('/')
+  })
+})
 
 // what does the number signify?
 // A: port number!
